400c2e965fd18106f0d11d28c8302d98d97ad4ac
Carter Schofield <72471291+CarterSchofield@users.noreply.github.com>
2022-04-24 15:01:41 -0600

    more graph progress

--- graph/main.py
+++ graph/main.py
@@ -1,4 +1,23 @@
 import graph
 
 def main():
-    f = open("graphData.txt")
+    f = open("data.txt")
+    numV = int(f.readline())
+    G = graph.Graph(numV)
+    numE = int(f.readline())
+    for i in range(numE):
+        line = f.readline()
+        words = line.split()
+        v0 = int(words[0])
+        v1 = int(words[1])
+        G.addEdge(v0, v1)
+    numT = int(f.readline())
+    for i in range(numT):
+        line = f.readline()
+        words = line.split()
+        v0 = int(words[0])
+        v1 = int(words[1])
+        G.findPath(v0, v1)
+
+if __name__ == "__main__":
+    main()
