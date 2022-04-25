79ed557ee1484346c9ed12b1a00778aa268de0f6
Carter Schofield <72471291+CarterSchofield@users.noreply.github.com>
2022-04-24 23:26:35 -0600

    Graph

--- graph/graph.py
+++ graph/graph.py
@@ -1,10 +1,11 @@
 from queue import Empty
+import queue
 
 class Graph:
     def __init__(self, numVertices):
         self.mNeighbors = []
         for i in numVertices:
-            pass
+            self.mNeighbors.append([])
 
     def addEdge(self, v0, v1):
         self.mNeighbors[v0].append(v1)
@@ -16,7 +17,7 @@ class Graph:
         pass
 
     def findPath(self, v0, v1):
-        Q = Queue()
+        Q = queue.Queue()
         xfrom = [-1] * len(self.mNeighbors)
         xfrom [v0] = v0
         Q.enqueue(v0)
